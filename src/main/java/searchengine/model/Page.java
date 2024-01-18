2015173c7614369c4e824d9a9d25e8b8bd4a794b
alex90bar <aleksandr.baranov@skillbox.ru>
2024-01-18 18:37:01 +0200

    Создал классы сущностей Page и Site

---
+++ src/main/java/searchengine/model/Page.java
@@ -0,0 +1,51 @@
+package searchengine.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * Page
+ *
+ * @author alex90bar
+ */
+
+@Entity
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "page")
+public class Page {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "site_id", nullable = false)
+    private Site siteId;
+
+    @Column(name = "path", columnDefinition = "TEXT NOT NULL, Index(path(512))")
+    private String path;
+
+    @Column(name = "code", nullable = false)
+    private Integer code;
+
+    @Column(name = "content", columnDefinition = "MEDIUMTEXT", nullable = false)
+    private String content;
+}
---
+++ src/main/java/searchengine/model/Site.java
@@ -0,0 +1,52 @@
+package searchengine.model;
+
+import java.time.ZonedDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * Site
+ *
+ * @author alex90bar
+ */
+
+@Entity
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "site")
+public class Site {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "status", columnDefinition = "ENUM('INDEXING', 'INDEXED', 'FAILED')", nullable = false)
+    private IndexingStatus status;
+
+    @Column(name = "status_time", nullable = false)
+    private ZonedDateTime statusTime;
+
+    @Column(name = "last_error", columnDefinition = "TEXT")
+    private String lastError;
+
+    @Column(name = "url", columnDefinition = "VARCHAR(255)", nullable = false)
+    private String url;
+
+    @Column(name = "name", columnDefinition = "VARCHAR(255)", nullable = false)
+    private String name;
+}
